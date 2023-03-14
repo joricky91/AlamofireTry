0111d0f2bdb2cbeaad1e2987169f428da6d15a07
joricky91 <jonathanricky91@gmail.com>
2023-03-14 11:09:46 +0700

    feat: creating network request in network service

---
+++ AlamofireTry/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  AlamofireTry
+//
+//  Created by Jonathan Ricky Sandjaja on 14/03/23.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let username: String
+    let email: String
+    let imageURL: String
+}
---
+++ AlamofireTry/Services/NetworkService.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkService.swift
+//  AlamofireTry
+//
+//  Created by Jonathan Ricky Sandjaja on 14/03/23.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class NetworkService {
+    
+    func loginUser(username: String, password: String, completion: @escaping (_ data: User) -> Void) {
+        let baseURL = "https://dummyjson.com/auth/login"
+        let headers: HTTPHeaders = ["Content-Type": "application/json"]
+        
+        var params: [String: String] = [:]
+        if username != "" && password != "" {
+            params["username"] = username
+            params["password"] = password
+        }
+        
+        AF.request(baseURL, method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+            do {
+                var userResponse: User?
+                if response.response?.statusCode == 200 {
+                    userResponse = try JSONDecoder().decode(User.self, from: response.data ?? Data())
+                    guard let user = userResponse else { return }
+                    completion(user)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
